4c93561eaa68ff579b5c6fef7ac9a14be2a84312
moataz <moatazemad772@gmail.com>
2023-03-07 22:03:10 +0200

    fixed reset password

--- routes/UserRoutes/resetPassword.js
+++ routes/UserRoutes/resetPassword.js
@@ -6,7 +6,7 @@ const router=express.Router()
 
 router.post('/reset-password', [
     body('email').isEmail().withMessage('Email must be valid'),
-    body('otp').notEmpty().withMessage('Please provide the correct otp'),
+    body('uniqueString').notEmpty().withMessage('Please provide the correct otp'),
     body('newpass').trim().isLength({min:6,max:255}).withMessage('Password must be at least 6 chars long and 255 max'),
 ],validatereq,resetPassword)
 module.exports={reset_password:router}
