45829ff7bf6f7fd394127dc97e6f9272c43bf8fa
moataz <moatazemad772@gmail.com>
2023-03-27 03:01:27 +0200

    only msg in error

--- middlewares/handelError.js
+++ middlewares/handelError.js
@@ -2,7 +2,9 @@ const handelerr=(err,req,res,next)=>{
     let transformed;
     let errtransformed;
     transformed=err.summary()
-    errtransformed={statusCode:err.statusCode,status:false,msg:transformed[0].message}
+    // errtransformed={statusCode:err.statusCode,status:false,msg:transformed[0].message}
+    errtransformed={msg:transformed[0].message}
+
     return res.status(err.statusCode).send(errtransformed)
 }
 module.exports={handelerr}
