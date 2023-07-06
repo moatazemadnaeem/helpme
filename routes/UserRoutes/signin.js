45bd926e8abd135f7b232fafde65aa440c197bbf
moataz <moatazemad772@gmail.com>
2023-07-07 01:15:13 +0300

    added role in signin

--- routes/UserRoutes/signin.js
+++ routes/UserRoutes/signin.js
@@ -4,11 +4,15 @@ const {validatereq}=require('../../middlewares/validateReq')
 const {body}= require('express-validator') 
 
 const router=express.Router()
-
+function validateRole(value) {
+    return ['Client', 'Technical'].includes(value);
+};
 router.post('/signin',
 [
     body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().isLength({min:6,max:255}).withMessage('Password must be at least 6 chars long and 255 max')
+    body('password').trim().isLength({min:6,max:255}).withMessage('Password must be at least 6 chars long and 255 max'),
+    body('role').custom(validateRole).withMessage('role must be either technical or client'),
+
 ],
 validatereq,
 signin
